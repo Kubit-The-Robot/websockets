bf8472c566858cbed152fa3a79f85c7846023300
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2020-10-30 02:38:08 -0300

    default commit

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const https = require('https');
-const WebSocket = require('ws');
+const express = require('express')
+const http = require('http')
+const WebSocket = require('ws')
 
-const port = 6969;
-const server = https.createServer(express);
-const wss = new WebSocket.Server({ server });
+const port = process.env.PORT || 8080;
+const app = express();
+const httpServer = http.createServer(app);
+
+const wss = new WebSocket.Server({
+  'server': httpServer
+});
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
@@ -16,6 +20,6 @@ wss.on('connection', function connection(ws) {
   })
 })
 
-server.listen(port, function() {
+httpServer.listen(port, function() {
   console.log(`Server is listening on ${port}!`)
 });
