e979bf9af1f7722682f1435d7371dafc3033d4f1
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2020-10-30 01:15:43 -0300

    🎉 Initial commit

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const express = require('express')
+const app = express()
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-const port = 9000;
-const server = http.createServer(express);
-const wss = new WebSocket.Server({ server })
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    })
+io.on('connection', (client) => {
+  console.log('a user connected')
+
+  client.on('chat', (data) => {
+    console.log('Message received -->', data)
+
+    io.emit('chat', data)
   })
-})
+});
 
-server.listen(port, function() {
-  console.log(`Server is listening on ${port}!`)
+
+// Now make our new WS server listen to port 5000
+io.listen(5000, () => {
+  console.log('Listening ... 🚀 ')
 })
