0cca2a04d7c349b14315d12dc580062c686d39c5
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2020-10-30 01:23:10 -0300

    ➕ Add dependency

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+app.get('/server', (req, res) => {
+  res.send('kubit is watching you...');
+});
 
 io.on('connection', (client) => {
-  console.log('a user connected')
+  console.log('a user connected');
 
   client.on('chat', (data) => {
-    console.log('Message received -->', data)
+    console.log('Message received -->', data);
 
-    io.emit('chat', data)
+    io.emit('chat', data);
   })
 });
 
-
 // Now make our new WS server listen to port 5000
 io.listen(5000, () => {
-  console.log('Listening ... 🚀 ')
-})
+  console.log('Listening ... 🚀 ');
+});
